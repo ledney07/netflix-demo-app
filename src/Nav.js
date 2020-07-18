0b5ae693b2533c52bed3fac8d00dc53dbc4b86cd
ledney07 <ledney21@gmail.com>
2020-07-17 20:40:30 -0400

    new react app commit

---
+++ src/Nav.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import "./css/Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <h1 className="nav__logo">Nin9</h1>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const APIKEY = "b232d4ab5eeedfaf2faa75ec3ede4477";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+  fetchActionMovies: `discover/movie?api_key=${APIKEY}&with_genres=28`,
+  fetchComedyMovies: `discover/movie?api_key=${APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `discover/movie?api_key=${APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `discover/movie?api_key=${APIKEY}&with_genres=10749`,
+  fetchDocumentaries: `discover/movie?api_key=${APIKEY}&with_genres=99`,
+};
+
+export default requests;
